bc5ac7278e65a396b0b4b0831067651bf55c6119
Net Wizard <internet.wizard@icloud.com>
2023-06-27 01:31:39 +0300

    feat: add basic functionality

---
+++ src/cd.js
@@ -0,0 +1,19 @@
+import path from "path";
+import fs from "fs";
+
+const cdr = async (dirName, workingDirectory) => {
+  const currentDirectory = path.resolve(workingDirectory, dirName);
+  try {
+    await fs.promises.access(currentDirectory, fs.constants.F_OK);
+    console.log(`You are currently in ${currentDirectory}`);
+    return currentDirectory;
+  } catch (error) {
+    console.log(
+      `Operation failed: sorry, there is no this directory ${currentDirectory}`
+    );
+    console.log(`You are currently in ${workingDirectory}`);
+    return workingDirectory;
+  }
+};
+
+export { cdr };
---
+++ src/index.js
@@ -0,0 +1,51 @@
+import readline from "readline";
+import os from "os";
+import { up } from "./up.js";
+import { output } from "./ls.js";
+import { cdr } from "./cd.js";
+
+let workingDirectory = os.homedir();
+
+const launchFileManager = async () => {
+  try {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    const username = process.argv
+      .find((a) => a.startsWith("--username="))
+      .split("=")
+      .pop();
+    console.log(`Welcome to the File Manager, ${username}!`);
+    console.log(`You are currently in ${workingDirectory}`);
+
+    rl.on("line", async (line) => {
+      const [command, ...args] = line.trim().split(" ");
+
+      switch (command) {
+        case "up":
+          workingDirectory = up(workingDirectory);
+          break;
+        case "ls":
+          await output(workingDirectory);
+          break;
+        case "cd":
+          workingDirectory = await cdr(args[0], workingDirectory);
+          break;
+        default:
+          console.log("Invalid input");
+          break;
+      }
+    });
+
+    rl.on("close", () => {
+      console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+      process.exit(0);
+    });
+  } catch (err) {
+    console.log("Operation failed");
+  }
+};
+
+await launchFileManager();
