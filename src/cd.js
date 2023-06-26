c200d35d7e381434af2c51727faeb98f43d70c97
Net Wizard <internet.wizard@icloud.com>
2023-06-27 02:09:47 +0300

    refactor: improve readability in change dir function

--- src/cd.js
+++ src/cd.js
@@ -1,10 +1,10 @@
 import path from "path";
-import fs from "fs";
+import { access } from "node:fs/promises";
 
 const cdr = async (dirName, workingDirectory) => {
   const currentDirectory = path.resolve(workingDirectory, dirName);
   try {
-    await fs.promises.access(currentDirectory, fs.constants.F_OK);
+    await access(currentDirectory, fs.constants.F_OK);
     console.log(`You are currently in ${currentDirectory}`);
     return currentDirectory;
   } catch (error) {
